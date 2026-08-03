2733d6fc4af5792b36780d345faa789b1e8c8427
Jini <dingguda1@gmail.com>
2026-08-04 01:20:34 +0900

    feat: add Spring Boot backend setup

---
+++ backend/src/main/java/com/jinhani/repofit/RepofitBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jinhani.repofit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepofitBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepofitBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/jinhani/repofit/RepofitBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinhani.repofit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepofitBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
